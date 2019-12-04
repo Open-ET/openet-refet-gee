58cc3112b7dcbad5a7555ac6a43287289dab6ed4
Charles Morton <charles.g.morton@gmail.com>
2019-12-04 15:43:08 -0800

    Added support for "use_cloud_api=True"

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,26 @@
-import ee
+import base64
+import logging
+import os
 
-ee.Initialize()
+import ee
+import pytest
+
+
+def pytest_configure():
+    # Called before tests are collected
+    # https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_sessionstart
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    logging.getLogger('googleapiclient').setLevel(logging.ERROR)
+    logging.debug('Test Setup')
+
+    # On Travis-CI authenticate using private key environment variable
+    if 'EE_PRIVATE_KEY_B64' in os.environ:
+        print('Writing privatekey.json from environmental variable ...')
+        content = base64.b64decode(os.environ['EE_PRIVATE_KEY_B64']).decode('ascii')
+        GEE_KEY_FILE = 'privatekey.json'
+        with open(GEE_KEY_FILE, 'w') as f:
+            f.write(content)
+        ee.Initialize(ee.ServiceAccountCredentials('', key_file=GEE_KEY_FILE),
+                      use_cloud_api=True)
+    else:
+        ee.Initialize(use_cloud_api=True)
