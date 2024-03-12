ba5a52b3205444ec6dd62f50e7c0483b3aeaec89
Charles Morton <cgmorton@users.noreply.github.com>
2024-03-12 13:44:49 -0700

    Bumping minimum supported Python version to 3.8

--- openet/refetgee/__init__.py
+++ openet/refetgee/__init__.py
@@ -1,12 +1,8 @@
-# try:
-#     from importlib import metadata
-# except ImportError:  # for Python<3.8
-#     import importlib_metadata as metadata
-
 from .daily import Daily
 from .hourly import Hourly
 
-# # __version__ = metadata.version(__package__ or __name__)
+# from importlib import metadata
 # __version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
+# # __version__ = metadata.version(__package__ or __name__)
 # # __version__ = metadata.version('openet-refet-gee')
 
